192882dea58c2a0723ad9dce765ce606da623bb9
Hitsukaya Valentaizar <87619517+Hitsukaya@users.noreply.github.com>
2025-04-17 22:09:59 +0300

    Update build_script.bat

--- build_script.bat
+++ build_script.bat
@@ -8,12 +8,12 @@ echo Build completed!
 
 :: Sign the executable
 "C:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x64\signtool.exe" sign ^
-    /f "C:\Users\Hitsukaya\Desktop\certifcate-launcher-launcher\certificat.pfx" ^
+    /f "C:\Users\Hitsukaya\Desktop\certifcate-launcher\certificat.pfx" ^
     /p nyx2123 ^
     /tr http://timestamp.digicert.com ^
     /td sha256 ^
     /fd sha256 ^
-    "C:\Users\Hitsukaya\Desktop\certifcate-launcher-launcher\Metin2Release.exe"
+    "C:\Users\Hitsukaya\Desktop\certifcate-launcher\Metin2Release.exe"
 
 echo Launcher signed successfully!
 pause
